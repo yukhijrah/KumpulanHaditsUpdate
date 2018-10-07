f22a46f65999f8866d9c64c3099d2581e49514e4
cartridsmith <yapimda@gmail.com>
2018-10-07 22:04:27 +0700

    Initial commit

---
+++ app/src/main/java/kumpulanhadits/app/kumpulanhadits/SplashActivity.java
@@ -0,0 +1,23 @@
+package kumpulanhadits.app.kumpulanhadits;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+
+}
